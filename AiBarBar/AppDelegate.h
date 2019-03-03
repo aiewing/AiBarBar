59d4ff26f4f695b492c33c745570f3aac1e0fb01
Aiewing <815610827@qq.com>
2019-03-04 01:09:20 +0800

    first commit

---
+++ AiBarBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AiBarBar
+//
+//  Created by Aiewing on 2019/3/3.
+//  Copyright © 2019 Aiewing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AiBarBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AiBarBar
+//
+//  Created by Aiewing on 2019/3/3.
+//  Copyright © 2019 Aiewing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
