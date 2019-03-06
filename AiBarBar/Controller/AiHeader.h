7a03486bc1ea9c88de4fa4e8cbdc61ddf72abc5f
Aiewing <815610827@qq.com>
2019-03-07 01:59:22 +0800

    修改自定义颜色

--- AiBarBar/Controller/AiHeader.h
+++ AiBarBar/Controller/AiHeader.h
@@ -12,4 +12,7 @@
 #define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
 #define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
 
+#define RGBA(r,g,b,a) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:a]
+#define ColorHex(s) [UIColor colorWithRed:(((s & 0xFF0000) >> 16))/255.0 green:(((s &0xFF00) >>8))/255.0 blue:((s &0xFF))/255.0 alpha:1.0]
+
 #endif /* AiHeader_h */
--- AiBarBar/Controller/AiTabBarController.m
+++ AiBarBar/Controller/AiTabBarController.m
@@ -9,9 +9,18 @@
 #import "AiTabBarController.h"
 #import "AiHeader.h"
 
+
+#define TitleDefaultColor RGBA(166, 166, 166, 1)
+#define SelectTitleDefaultColor RGBA(69, 153, 247, 1)
+
 @interface AiTabBarController ()
 
 @property (nonatomic, strong) UIImageView *bgImgView;
+
+@property (nonatomic, assign) NSInteger tabBarItemCount;
+@property (nonatomic, strong) NSArray<UIViewController *> * subControllers;
+@property (nonatomic, strong) NSArray<UIColor *> * titleColors;
+@property (nonatomic, strong) NSArray<UIColor *> * selectTitleColors;
 
 @end
 
@@ -29,6 +38,9 @@
                            images:(NSArray<UIImage *> *)imgs
                      selectImages:(NSArray<UIImage *> *)selectImgs {
     
+    self.subControllers = ctls;
+    self.tabBarItemCount = MIN(ctls.count, 5);
+    
     for (int i = 0; i < ctls.count; i++) {
         UIViewController *ctl = ctls[i];
         ctl.title = titles[i];
@@ -39,13 +51,17 @@
     self.tabBar.hidden = true;
     [self.view addSubview:self.bgImgView];
     
-    CGFloat subViewWidth = SCREEN_WIDTH / ctls.count;
-    for (int i = 0; i < ctls.count; i++) {
+    CGFloat subViewWidth = SCREEN_WIDTH / self.tabBarItemCount;
+    NSMutableArray *titleColorArr = [NSMutableArray array];
+    NSMutableArray *selectTitleColorArr = [NSMutableArray array];
+    for (int i = 0; i < self.tabBarItemCount; i++) {
         UIView *subView = [[UIView alloc] initWithFrame:CGRectMake(subViewWidth * i, 0, subViewWidth, 49)];
         [self.bgImgView addSubview:subView];
+        subView.tag = i + 100;
         
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, subView.frame.size.height - 15, subViewWidth, 15)];
         [subView addSubview:label];
+        label.tag = i + 120;
         label.userInteractionEnabled = true;
         label.text = titles[i];
         label.textAlignment = NSTextAlignmentCenter;
@@ -53,6 +69,7 @@
         
         UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
         [subView addSubview:imgView];
+        imgView.tag = i + 140;
         imgView.userInteractionEnabled = true;
         imgView.image = imgs[i];
         imgView.center = CGPointMake(subView.frame.size.width / 2, subView.frame.size.height / 2 - 5);
@@ -60,6 +77,29 @@
         // 添加手势
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tabBarTap:)];
         [subView addGestureRecognizer:tap];
+        
+        if (titleColors[i]) {
+            [titleColorArr addObject:titleColors[i]];
+        } else {
+            if (titleColors.firstObject) {
+                [titleColorArr addObject:titleColors.firstObject];
+            } else {
+                [titleColorArr addObject:TitleDefaultColor];
+            }
+        }
+        
+        if (selectTitleColors[i]) {
+            [selectTitleColorArr addObject:selectTitleColors[i]];
+        } else {
+            if (selectTitleColors.firstObject) {
+                [selectTitleColorArr addObject:selectTitleColors.firstObject];
+            } else {
+                [selectTitleColorArr addObject:SelectTitleDefaultColor];
+            }
+        }
+        
+        self.titleColors = titleColorArr;
+        self.selectTitleColors = selectTitleColorArr;
     }
 }
 
@@ -67,10 +107,33 @@
 - (void)viewWillLayoutSubviews {
     
     self.bgImgView.frame = CGRectMake(0, SCREEN_HEIGHT - 49, SCREEN_WIDTH, 49);
+    
+    CGFloat subViewWidth = SCREEN_WIDTH / self.tabBarItemCount;
+    for (int i = 0; i < self.tabBarItemCount; i++) {
+        UIView *subView = [self.bgImgView viewWithTag:i + 100];
+        subView.frame = CGRectMake(subViewWidth * i, 0, subViewWidth, 49);
+        
+        UILabel *label = (UILabel *)[self.bgImgView viewWithTag:i + 120];
+        label.frame = CGRectMake(0, subView.frame.size.height - 15, subViewWidth, 15);
+        
+        UIImageView *imgView = (UIImageView *)[self.bgImgView viewWithTag:i + 140];
+        imgView.center = CGPointMake(subView.frame.size.width / 2, subView.frame.size.height / 2 - 5);
+    }
 }
 
 - (void)tabBarTap:(UITapGestureRecognizer *)tap {
+    // 点击的时候字体需要有变化
+    for (int i = 0; i < self.tabBarItemCount; i++) {
+        UILabel *label = (UILabel *)[self.bgImgView viewWithTag:i + 120];
+        if (label.tag == tap.view.tag + 20) {
+            label.textColor = self.selectTitleColors[i];
+        } else {
+            label.textColor = self.titleColors[i];
+        }
+    }
     
+    // 切换控制器
+    self.selectedIndex = tap.view.tag - 100;
 }
 
 #pragma mark - Lazy
